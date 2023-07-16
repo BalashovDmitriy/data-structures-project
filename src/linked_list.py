9fb59a84dc75a1f7163e7672a6f8e8a81a90903f
dmitriy.balashov <reaver_std@mail.ru>
2023-07-16 10:58:03 +0500

    add code homework-6

--- src/linked_list.py
+++ src/linked_list.py
@@ -24,11 +24,36 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
 
-    def insert_at_end(self, data: dict) -> None:
+    def insert_at_end(self, data) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
         new_node = Node(data)
         self.tail.next_node = new_node
         self.tail = new_node
+
+    def to_list(self):
+        """Вывод данных односвязного списка в список"""
+        node = self.head
+        if node is None:
+            return None
+        ll_list = []
+        while node:
+            ll_list.append(node.data)
+            node = node.next_node
+        return ll_list
+
+    def get_data_by_id(self, id_: int):
+        """Возвращает данные (словарь) узла с указанным id"""
+        node = self.head
+        if node is None:
+            return None
+        while node:
+            try:
+                if node.data['id'] == id_:
+                    return node.data
+                node = node.next_node
+            except TypeError:
+                print("Данные не являются словарем или в словаре нет id.")
+                node = node.next_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -13,3 +13,24 @@ class TestLinkedList(unittest.TestCase):
         ll.insert_at_end({'id': 3})
         ll.insert_beginning({'id': 0})
         self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_linked_list_to_list(self):
+        ll = LinkedList()
+
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        ll.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        ll.insert_beginning({'id': 0, 'username': 'serebro'})
+        lst = ll.to_list()
+        self.assertEqual(lst[0], {'id': 0, 'username': 'serebro'})
+        self.assertEqual(lst[3], {'id': 3, 'username': 'mosh_s'})
+
+    def test_linked_list_get_by_data_id(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end('idusername')
+        ll.insert_at_end([1, 2, 3])
+        ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+
+        user_data = ll.get_data_by_id(2)
+        self.assertEqual(user_data, {'id': 2, 'username': 'mosh_s'})
